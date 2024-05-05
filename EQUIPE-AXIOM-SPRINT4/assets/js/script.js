604fa16bc63f3b222b1a19d3296695aacfea613a
eu-larissasouza <larissa.alves.souza@outlook.com>
2024-05-04 23:52:04 -0300

    Commit Final - Sprint 4 - Minigame

---
+++ EQUIPE-AXIOM-SPRINT4/assets/js/script.js
@@ -0,0 +1,109 @@
+// Buscando elementos do DOM
+const arena = document.getElementById('arena')
+const roboEva = document.getElementById('iconEva')
+const roboWallE = document.getElementById('iconWallE')
+const placarEva = document.getElementById('placarEva')
+const placarWallE = document.getElementById('placarWallE')
+
+// Definindo posição inicial dos robôs
+let roboEvaX = 0
+let roboEvaY = 0
+let roboWallEX = 750
+let roboWallEY = 450
+
+// Definindo o total de vida dos robôs
+let vidaEva = 100
+let vidaWallE = 100
+
+// Definindo constante para o limite de colisões
+const LIMITE_COLISOES = 5
+
+// Definindo variável que irá contar o total de colisões
+let totalColisoes = 0
+
+// Função para atualizar a posição do robô
+const atualizaPosicaoRobo = (robo, x, y) => {
+  robo.style.left = x + 'px'
+  robo.style.top = y + 'px'
+}
+
+// Função para atualizar a vida dos robôs
+const atualizaVida = () => {
+  vidaEva -= Math.floor(Math.random() * 20)
+  placarEva.textContent = vidaEva
+
+  vidaWallE -= Math.floor(Math.random() * 20)
+  placarWallE.textContent = vidaWallE
+}
+
+// Função para resetar a posição dos robôs após colisão
+const resetPosicoes = () => {
+  let roboEvaX = 0
+  let roboEvaY = 0
+  let roboWallEX = 750
+  let roboWallEY = 450
+
+  atualizaPosicaoRobo(roboEva, roboEvaX, roboEvaY)
+  atualizaPosicaoRobo(roboWallE, roboWallEX, roboWallEY)
+}
+
+// Função para verificar colisões
+const verificarColisões = () => {
+  const distancia = Math.sqrt(
+    (roboEvaX - roboWallEX) * 2 + (roboEvaY - roboWallEY) * 2
+  )
+
+  if (totalColisoes >= LIMITE_COLISOES) {
+    document.querySelector('.resultado').classList.add('active')
+    if (vidaEva < vidaWallE) {
+      // Eva ganhou
+    } else {
+      //Trocando a foto caso o robô vencedor seja o Wall-E
+      document.getElementById('roboVencedor').src = 'assets/img/iconWallE.png'
+    }
+  }
+
+  if (distancia < LIMITE_COLISOES) {
+    totalColisoes++
+    resetPosicoes()
+    atualizaVida()
+  }
+}
+
+//Função para fechar o modal inicial
+const NovaPartida = () => {
+  document.querySelector('.inicio').classList.add('inactive')
+  resetPosicoes()
+}
+
+//Função para recarregar tela após final do jogo
+const Voltar = () => {
+  window.location.reload()
+}
+
+// Movimento do robô com setas ou letras
+document.addEventListener('keydown', event => {
+  const { key } = event
+
+  if (key.toUpperCase() === 'A' && roboEvaX > 0) {
+    roboEvaX -= 10
+  } else if (key.toUpperCase() === 'W' && roboEvaY > 0) {
+    roboEvaY -= 10
+  } else if (key.toUpperCase() === 'S' && roboEvaY < 450) {
+    roboEvaY += 10
+  } else if (key.toUpperCase() === 'D' && roboEvaX < 750) {
+    roboEvaX += 10
+  } else if (key === 'ArrowLeft' && roboWallEX > 0) {
+    roboWallEX -= 10
+  } else if (key === 'ArrowUp' && roboWallEY > 0) {
+    roboWallEY -= 10
+  } else if (key === 'ArrowDown' && roboWallEY < 450) {
+    roboWallEY += 10
+  } else if (key === 'ArrowRight' && roboWallEX < 750) {
+    roboWallEX += 10
+  }
+
+  atualizaPosicaoRobo(roboEva, roboEvaX, roboEvaY)
+  atualizaPosicaoRobo(roboWallE, roboWallEX, roboWallEY)
+  verificarColisões()
+})
